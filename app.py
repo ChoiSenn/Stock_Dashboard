7b8b6f8fbd94e5108ee9a1b154196e16788c1430
ChoiSenn <90544732+ChoiSenn@users.noreply.github.com>
2023-12-04 22:22:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import openai
+from openai import OpenAI
 
 # --- 뉴스 기사 크롤링 ---------------------------------------------------------------
 

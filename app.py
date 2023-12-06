61145f36ae8495173a6f70fc255733f45c976238
ChoiSenn <90544732+ChoiSenn@users.noreply.github.com>
2023-12-06 15:08:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,12 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
 def graphPrint(df, df_medium, df_change, df_all):
   with tab1:
     st.subheader(select_stock + '의 최근 주가')
-    st.write('Open : 거래 시작 시간에 최초로 체결된 가격 \nHigh : 상한가 (주가 최고 상승폭 -> +30%) \nLow : 하한가 (주가 최고 하락폭 -> -30%) \nClose : 장이 마감되는 시간의 최종 체결 가격 \nVolume : 주식 거래량 \nChange : 주가 변동 폭')
+    st.write('Open : 거래 시작 시간에 최초로 체결된 가격')
+    st.write('High : 상한가 (주가 최고 상승폭 -> +30%)')
+    st.write('Low : 하한가 (주가 최고 하락폭 -> -30%)')
+    st.write('Close : 장이 마감되는 시간의 최종 체결 가격')
+    st.write('Volume : 주식 거래량')
+    st.write('Change : 주가 변동 폭')
     st.dataframe(df.tail())
 
     st.subheader(select_stock + '의 주가 변동')
@@ -80,7 +85,7 @@ def graphPrint(df, df_medium, df_change, df_all):
 
     st.subheader(select_stock + '의 캔들스틱 차트')
     fig1 = plt.figure()
-    mpf.plot(df, type='candle', style='yahoo', volume=True)
+    mpf.plot(df, style='yahoo', volume=True)
     st.pyplot(fig1)
 
     st.subheader(select_stock + '의 골든크로스 / 데드크로스')
